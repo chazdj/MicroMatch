fee7984eb8b4de2f7d02997b50b9f2584e187dc9
cdjoanem@gmail.com <cdjoanem@gmail.com>
2026-02-13 16:17:57 -0500

    User SQLAlchemy model implemented.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
-from app.database import engine
-from app import models
 from .database import engine
 from .models import Base
+from .database import SessionLocal
+from .models import User
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
     return {"message": "MicroMatch API running"}
-
-
-Base.metadata.create_all(bind=engine)
